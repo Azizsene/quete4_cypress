e491ef3342e3080f33d2c9d94b54fb5eecaf8e85
Aziz Sene <abdouaziz.sene@gmail.com>
2024-06-03 10:13:53 +0200

    Upload and Download

---
+++ cypress/e2e/Test.cy.js
@@ -0,0 +1,43 @@
+describe("Test chargement et telechargement", () => {
+  beforeEach(() => {
+    cy.visit("https://filebin.net/");
+  });
+
+  it("Telechargement au format Zip", () => {
+    cy.get("#fileField").attachFile("imgquete4.avif");
+    cy.contains("It contains 1 uploaded file").should("be.visible");
+    cy.contains("Download files").click();
+    cy.contains("Zip")
+      .invoke("attr", "href")
+      .then((downloadLink) => {
+        const absulteLink = "https://filebin.net/" + downloadLink;
+        cy.log(absulteLink);
+        cy.downloadFile(
+          absulteLink,
+          "mydownloads/zipFiles",
+          "downloadedFromCypress.zip"
+        );
+        cy.readFile("mydownloads/zipFiles/downloadedFromCypress.zip");
+      });
+  });
+
+  it("Telechargement au format Tar", () => {
+    cy.get("#fileField").attachFile("imgquete4.avif");
+    cy.contains("It contains 1 uploaded file").should("be.visible");
+    cy.contains("Download files").click();
+    cy.contains("Tar")
+      .invoke("attr", "href")
+      .then((downloadLink) => {
+        const absulteLink = "https://filebin.net/" + downloadLink;
+        cy.log(downloadLink);
+        cy.downloadFile(
+          absulteLink,
+          "mydownloads/tarFiles",
+          "downloadedFromCypress.tar"
+        );
+        cy.readFile("mydownloads/tarFiles/downloadedFromCypress.tar");
+
+      });
+      
+  });
+});
